7a5444bc31da057f46a0a376b0cd7e1c05e90f3e
Marina <m-fisun23@yandex.ru>
2023-03-08 20:16:53 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Task 1");
+        int contribution = 15000;
+        int total = 0;
+        int i = 0;
+        while ( total < 2_459_000 ) {
+            total = total + contribution;
+            System.out.println("Месяц " + i++ + " сумма накоплений равна " + total + "рублей");
+        }
     }
 }
