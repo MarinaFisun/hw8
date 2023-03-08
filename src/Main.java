8c21b692dd5c074ed60e07ecb019199d295393b8
Marina <m-fisun23@yandex.ru>
2023-03-08 20:44:18 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -10,7 +11,17 @@ public class Main {
         int i = 0;
         while ( total < 2_459_000 ) {
             total = total + contribution;
-            System.out.println("Месяц " + i++ + " сумма накоплений равна " + total + "рублей");
+            System.out.println("Месяц " + i++ + " сумма накоплений равна " + total + " рублей");
         }
+    }
+    public static void task2() {
+        System.out.println("Task 2");
+        int i = 1;
+        while ( i <= 10 ) {
+            System.out.print(i++ +" ");
+        }
+        System.out.println();
+        for (int k = 10 ; k >= 1; k--)
+            System.out.print(k + " ");
     }
 }
